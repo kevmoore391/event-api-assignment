372820473f8f9b4f5ad18a7ce7f629bb81a40f72
moorek <kevin.moore1@dell.com>
2021-12-30 12:04:08 +0000

    remove packages

--- Controllers/EventController.cs
+++ Controllers/EventController.cs
@@ -44,7 +44,6 @@ namespace EventApiAssignment.Controllers
         }
 
         // PUT: Event/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
         public async Task<IActionResult> PutEvent(string id, Event eventToUpdate)
         {
@@ -87,7 +86,6 @@ namespace EventApiAssignment.Controllers
         }
 
         // POST: Event
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
         public async Task<ActionResult<Event>> PostEventItem(Event eventToSave)
         {
